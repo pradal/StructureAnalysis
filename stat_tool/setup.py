e0b9f2b0fa9023f1d4bcfc4c376905b6ef028b48
pradal <christophe.pradal@inria.fr>
2016-09-07 09:20:47 +0200

    test alternative setup for conda

--- stat_tool/setup.py
+++ stat_tool/setup.py
@@ -1,30 +1,26 @@
 # -*- coding: utf-8 -*-
 """setup file for stat_tool package"""
 
-import os, sys
-from setuptools import setup, find_packages
-from openalea.deploy.binary_deps import binary_deps
-from openalea.deploy.setup import *
-from os.path import join as pj
 try:
     from openalea.deploy.util import is_conda_env
 except ImportError:
     def is_conda_env():
         return False
 
+
+import os, sys
+from setuptools import setup, find_packages
 from openalea.deploy.metainfo import read_metainfo
 
 metadata = read_metainfo('metainfo.ini', verbose=True)
 for key,value in metadata.iteritems():
     exec("%s = '%s'" % (key, value))
 
-if is_conda_env():
-    # Do not build with scons
-    build_prefix = None
-    scons_parameters = []
-    install_requires = []
-    setup_requires = ['openalea.deploy']
-else:
+if not is_conda_env():
+    from openalea.deploy.binary_deps import binary_deps
+    from openalea.deploy.setup import *
+    from os.path import join as pj
+
     build_prefix = "build-scons"
 
     # Scons build directory
@@ -37,49 +33,10 @@ else:
         install_requires += [binary_deps("boost")]
     install_requires = []
 
-setup_requires = install_requires + ['openalea.deploy']
+    setup_requires = install_requires + ['openalea.deploy']
 
+    if __name__ == '__main__':
 
-if __name__ == '__main__':
-
-    if is_conda_env():
-        setup(name=name,
-              version=version,
-              description=description,
-              long_description=long_description,
-              author=authors,
-              author_email=authors_email,
-              url=url,
-              license=license,
-              platforms = platforms,
-
-              namespace_packages=['openalea'],
-              create_namespaces=True,
-
-              # Packages
-              packages=['openalea',
-                        'openalea.stat_tool',
-                        ],
-
-              package_dir={ "openalea.stat_tool" : pj("src","stat_tool"), '':'src'  },
-              share_dirs = { 'share' : 'share' },
-
-
-              # Add package platform libraries if any
-              include_package_data=True,
-              package_data = {'' : ['*.pyd', '*.so', '*.dylib', '*.png', '*.hsc', '*.seq', '*.aml'],},
-
-              zip_safe = False,
-
-              # Dependencies
-              setup_requires = setup_requires,
-              install_requires = install_requires,
-              dependency_links = ['http://openalea.gforge.inria.fr/pi'],
-
-
-           )
-
-    else:
         setup(name=name,
               version=version,
               description=description,
@@ -125,6 +82,44 @@ if __name__ == '__main__':
 
 
            )
+
+else:
+  # is_conda_env
+  print ("Setup with conda")
+  from setuptools import setup, find_packages
+
+  if __name__ == '__main__':
+
+      setup(name=name,
+            version=version,
+            description=description,
+            long_description=long_description,
+            author=authors,
+            author_email=authors_email,
+            url=url,
+            license=license,
+            platforms = platforms,
+
+            namespace_packages=['openalea'],
+            create_namespaces=True,
+
+            # Packages
+            packages=['openalea',
+                      'openalea.stat_tool',
+                      ],
+
+            package_dir={ "openalea.stat_tool" : pj("src","stat_tool"), '':'src'  },
+            share_dirs = { 'share' : 'share' },
+
+
+            # Add package platform libraries if any
+            include_package_data=True,
+            package_data = {'' : ['*.pyd', '*.so', '*.dylib', '*.png', '*.hsc', '*.seq', '*.aml'],},
+
+            zip_safe = False,
+
+
+         )
 
 
 
